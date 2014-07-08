b18fdc28940e47e00365095c3cb712d66c89f0ed
vnabet <vnabet@gmail.com>
2014-07-08 14:22:54 +0200

    Server app

--- server/app/app.js
+++ server/app/app.js
@@ -1,18 +1,46 @@
-/*var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('../db/HoN');
+(function () {
+  "use strict";
+  
+  var express = require('express');
+  var app = express();
+  var bodyParser = require('body-parser');
+  var sqlite3 = require('sqlite3').verbose();
+  
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  
+  var port = process.env.PORT || 8180;
+  var dbfile = "../db/HoN.sqlite";
+  
+  var router = express.Router();
+  
+  var db = new sqlite3.Database(dbfile, sqlite3.OPEN_READ);
+  
 
-db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
-
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
-
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
+  router.use(function (req, res, next) {
+    console.log('something is happening');
+    next();
   });
-});
-
-db.close();*/
+  
+  router.get('/', function (req, res) {
+    res.json({message: 'coucou'});
+  });
+  
+  
+  router.route('/:game_id/:package_id/tiles').get(function (req, res) {
+    
+    db.all('SELECT * FROM tile WHERE packageid = $packageid', {$packageid: req.params.package_id},  function (error, rows) {
+      res.json(rows);
+    });
+    
+    
+  });
+  
+  
+  app.use('/api', router);
+  
+  app.listen(port);
+  
+  console.log('Serveur en cours sur le port ' + port);
+  
+}());
